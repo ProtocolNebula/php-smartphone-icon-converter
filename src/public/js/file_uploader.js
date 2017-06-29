f16354ee1b2b0ef8e63d264e5b66a2e8d83b1e61
ProtocolNebula <powergm@hotmail.es>
2017-06-29 13:29:27 +0200

    Big files blocked via javascript

--- src/public/js/file_uploader.js
+++ src/public/js/file_uploader.js
@@ -1,9 +1,29 @@
-var activities = document.getElementsByName("icon")[0];
+var fileUpload = document.getElementsByName("icon")[0];
 
-activities.addEventListener("change", function() {
-	checkFile();
+fileUpload.addEventListener("change", function() {
+	return checkFile();
 });
 
 function checkFile() {
+	file = fileUpload.files[0];
+	if (checkFileSize(file) && checkFileExtension(file)) {
+		return true;
+	}
 	
+	// ERROR
+	fileUpload.value = "";
+}
+
+function checkFileSize(file) {
+	if (file.size > MAX_FILE_SIZE) {
+		alert("File \""+file.name+"\" is too large");
+		return false;
+	}
+	
+	return true;
+}
+
+function checkFileExtension(file) {
+	// TODO: Check extensions (must be received from php)
+	return true;
 }
